4f9c686dd3c36682570e04773b9d5d6a4742e4a6
dietricherwane <dietricherwane@yahoo.fr>
2010-10-05 11:48:22 +0000

    Upload on heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fleetservice::Application.config.secret_token = '44456f0d046e60bbcc1395f7749b057982d7a41ea03840abfa331a0bdbba2d0d9794fd0997c0e45087efab36a6b06a94ede060210249a17172d085b63db43ca8'
